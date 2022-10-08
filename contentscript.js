53d9cc6135a519d449e4aea1e1e0f29e11fbfb19
BuildTools <unconfigured@null.spigotmc.org>
2022-10-08 17:18:54 +0200

    tussentijdse commit 2

--- contentscript.js
+++ contentscript.js
@@ -1,82 +1,69 @@
 // Execute code when DOM is loaded
 window.onload = async function() {
 
-    // Get video image
     const frontImageDiv = await waitForElm(".inset-video-item-play-action-container");
+    const frontImage = await waitForElm(".inset-video-item-image");
 
-    // Wait for click on image to init video buttons
-    frontImageDiv.addEventListener("click", async function() {
+    // frontImageDiv.addEventListener("click", async function() {
+    //     await tmButtonStuff();
+    // });
 
-        // Get video control bar
-        let controlbarBottom = await waitForElm("#bmpui-id-77");
-        let controlbarDiv = controlbarBottom.children[0];
-
-        // Make our own button
-        let tmButton = document.createElement("button"); // bmpui-ui-fullscreentogglebutton
-        tmButton.className = "bmpui-ui-fullscreentogglebutton tmbutton bmpui-off";
-        tmButton.id = "tmButton"
-        tmButton.type = "button";
-        tmButton.addEventListener("click", async function() {
-
-            let tempTMButton = await waitForElm("#tmButton");
-            let player = await waitForElm(".inset-video-item-image-container");
-            let header = await waitForElm(".header");
-            let playButton = await waitForElm(".inset-video-item-play-button-container");
-
-            // Toggle functionality
-            if(tempTMButton.classList.contains("bmpui-off")){
-                // TM ON
-                tempTMButton.classList.remove("bmpui-off");
-                tempTMButton.classList.add("bmpui-on");
-
-                player.classList.add("tmlargeplayer");
-                header.classList.add("tmhidden");
-                playButton.classList.add("tmhidden");
-
-            } else if(tempTMButton.classList.contains("bmpui-on")){
-                // TM OFF
-                tempTMButton.classList.remove("bmpui-on");
-                tempTMButton.classList.add("bmpui-off");
-
-                player.classList.remove("tmlargeplayer");
-                header.classList.remove("tmhidden");
-                playButton.classList.remove("tmhidden");
-
-            }
-        })
-
-        // Add our button to the control bar
-        controlbarDiv.appendChild(tmButton);
-
-
-
-        // Make new style for large player with class TMLargePlayer
-        let customStyle = document.createElement("style");
-        customStyle.innerHTML = `
-            .tmbutton {
-            }
-        
-            .tmlargeplayer {
-                position: fixed;
-                top: 0;
-                left: 50%;
-                width: 150%;
-                aspect-ratio: 16/9;
-                max-width: 98vw;
-                transform: translateX(-50%);
-            }
-            
-            .tmhidden {
-                display: none;
-            }
-        `;
-
-        // Add our style to the page
-        document.head.appendChild(customStyle);
-
+    frontImage.addEventListener("click", async function() {
+        await tmButtonStuff();
     })
 
 };
+
+
+async function tmButtonStuff() {
+    let controlbarBottom = await waitForElm(".bmpui-controlbar-bottom");
+    let controlbarDiv = controlbarBottom.children[0];
+
+    let tmButton = document.createElement("button");
+    tmButton.className = "bmpui-ui-fullscreentogglebutton bmpui-off";
+    tmButton.type = "button";
+    tmButton.id = "tmButton";
+    tmButton.addEventListener("click", async function () {
+
+        console.log("clicked");
+
+        let tempTMButton = await waitForElm("#tmButton")
+        // console.log("temptmbutton: " + tempTMButton)
+
+        let player = await waitForElm(".inset-video-item-image-container")
+        // console.log("player: " + player)
+
+        let header = await waitForElm(".header")
+        // console.log("header: " + header)
+
+        let playButton = await waitForElm(".inset-video-item-play-button-container")
+        // console.log("play button: " + playButton)
+
+        // console.log("components gathered, toggle should start now")
+        // Toggle functionality
+        if (tempTMButton.classList.contains("bmpui-off")) {
+            // TM ON
+            tempTMButton.classList.remove("bmpui-off");
+            tempTMButton.classList.add("bmpui-on");
+
+            player.classList.add("tmlargeplayer");
+            header.classList.add("tmhidden");
+            playButton.classList.add("tmhidden");
+
+        } else if (tempTMButton.classList.contains("bmpui-on")) {
+            // TM OFF
+            tempTMButton.classList.remove("bmpui-on");
+            tempTMButton.classList.add("bmpui-off");
+
+            player.classList.remove("tmlargeplayer");
+            header.classList.remove("tmhidden");
+            playButton.classList.remove("tmhidden");
+
+        }
+    })
+
+    controlbarDiv.appendChild(tmButton)
+}
 
 
 
@@ -87,7 +74,7 @@ async function waitForElm(selector) {
             return resolve(document.querySelector(selector));
         }
 
-        const observer = new MutationObserver(mutations => {
+        const observer = new MutationObserver(() => {
             if (document.querySelector(selector)) {
                 resolve(document.querySelector(selector));
                 observer.disconnect();
